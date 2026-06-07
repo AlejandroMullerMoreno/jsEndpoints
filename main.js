4c2c762ca5a028ba99e19dc5759bb12fd226ad7c
Alejandro Moreno <alejandromorenomuller@gmail.com>
2026-06-07 17:54:51 -0500

    finish setting up html endpoint

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ import fs from "node:fs/promises";
 
 
 async function cumResponse(req,res) {
+    console.log(req.url)
+    // read the file, set the header, write the file contents to the body and send a response
+    if (req.url == "/html"){
+        const shisaResponseString = await fs.readFile("./media/shisa.html","utf8")
+        res.setHeader("Content-Type","text/html");
+        res.end(shisaResponseString)
+    }
+
+    else{
     // read the file, set the header, write the file contents to the body and send a response
     const cumResponseString = await fs.readFile("./media/cum.txt","utf8")
     res.setHeader("Content-Type","text/html")
     res.end(cumResponseString)
+
+    }
 }
 const meinServer = http.createServer(cumResponse)
 
